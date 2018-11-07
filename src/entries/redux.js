6e25e38f06f31400781ee0c8e343867291ce259c
Freddy Castillo <freddy.geovanni@gmail.com>
2018-11-07 01:01:03 -0500

    FCA  - Flujo de Redux

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -6,22 +6,58 @@ function handleSubmit(event) {
     event.preventDefault();
     const data = new FormData($form);
     const title = data.get("title");
-
     console.log(title)
+    store.dispatch({
+        type: "ADD_SONG",
+        payload: {
+            title
+        }
+    })
+
 }
-const initialState=[
+const initialState = [
     {
-        "title":"Despacito"
+        "title": "Despacito"
     },
     {
-        "title":"One more thin"
+        "title": "One more thin"
     },
     {
-        "title":"Echame la culpa"
+        "title": "Echame la culpa"
     }
 ]
+const reducer = function (state, action) {
+    switch (action.type) {
+        case "ADD_SONG":
+            return [...state, action.payload]
+            break;
+        default:
+            return state;
+            break;
+    }
+}
 const store = createStore(
-    (estado)=>estado,
+    reducer,
     initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+
+function render() {
+    const $container = document.getElementById("playlist");
+    $container.innerHTML='';
+    const playList = store.getState();
+    playList.forEach(ele => {
+        const element = document.createElement('p');
+        element.textContent = ele.title;
+        $container.appendChild(element);
+        console.log(ele)
+    });
+}
+
+render();
+
+function handleChange() {
+    render();
+}
+store.subscribe(handleChange);
